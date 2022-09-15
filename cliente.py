0cc32a68b6a71316ce6a3b57ddc68c36f827d53e
AndreBBM <andrebbmelo@hotmail.com>
2022-09-15 15:10:48 -0300

    cliente e utils

--- cliente.py
+++ cliente.py
@@ -11,7 +11,7 @@ from random import randint
 import signal
 from util import *
 
-serialName = "COM5"
+serialName = "COM4"
 
 COMANDOS = [b'\x00\xFA\x00\x00', #1
             b'\x00\x00\xFA\x00', #2
@@ -58,6 +58,7 @@ def main():
 
         #send a handshake to the server with 10 bytes on the head
         handshake = create_package(handshake_head, b'\x00', end)
+        print("Handshake: {0}".format(handshake))
         send_package(com1, handshake)
 
         time.sleep(0.5)
@@ -65,7 +66,7 @@ def main():
         response = get_separeted_package(com1)
         time.sleep(0.2)
 
-        if response[0][1] == 255:
+        if response[0][0] == 255:
             print("Handshake recebido")
         else:
             print("Handshake não recebido")
@@ -74,20 +75,21 @@ def main():
 
         #send a package with n as payload
         print("Enviando o tamanho")
-        head[1] = [b'\x05']
+        head[1] = [b'\x04']
         package = create_package(head,[n], end)
+        print("Package: {0}".format(package))
         send_package(com1, package)
         time.sleep(0.1)
 
         numero_pacote = 0
         for i in lista_listas:
-
+            print("Enviando pacote {0}".format(numero_pacote))
             pacote_enviado_com_sucesso = False
             numero_certo = False
             tamanho_certo = False
             while pacote_enviado_com_sucesso == False:
                 print("Enviando comando")
-                total_size = len(i)
+                total_size = 0
                 for c in i:
                     total_size += len(c)
                 #send the command to the server
@@ -103,13 +105,13 @@ def main():
                 #get the response from the server
                 response = get_separeted_package(com1)
                 print("Recebeu a resposta")
-                if response[0][1] == 251:
+                if response[0][0] == 251:
                     print("tudo certo")
                     numero_certo = True
                 else:
                     print("numero errado")
                     print(response[0])
-                    print(response[0][1])
+                    print(response[0][0])
                     numero_certo = False
                 
 
@@ -124,7 +126,7 @@ def main():
                 print(f"Lista bytes: {i}")
 
                 verification = get_separeted_package(com1)
-                ttotal = verification[1].strip(b'\xcc').strip(b'\x00\x00\x00')
+                ttotal = verification[1].strip(b'\x00\x00\x00')
                 print("verificação recebida: {0}".format(verification))
                 print("Recebeu {} bytes".format(ttotal))
                 print("Recebido: {} bytes".format(int.from_bytes(ttotal, byteorder='big')))
@@ -141,6 +143,7 @@ def main():
                 time.sleep(0.2)
                 print("numero do pacote cuja operação foi realizada",numero_pacote)
                 print("☺"*100)
+                print(numero_certo, tamanho_certo)
                 if numero_certo == True and tamanho_certo == True:
                     pacote_enviado_com_sucesso = True
                     numero_pacote += 1
